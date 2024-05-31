8b0956a0c4dc14b3eff2ecd90e5068e64a59725b
Joan Corona <joan@minnekdigital.com>
2024-05-31 16:38:34 -0400

    feat: add slick carousel compatibility

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import $ from 'jquery';
+import 'slick-carousel';
 
 import { App } from './components/App/App';
+
+interface JqueryElement extends JQuery<HTMLElement> {
+    slick(options?: object | string): void;
+}
+
+$(() => {
+    const $slicks = $('[data-slick]');
+    if (!$slicks.length) return;
+    $slicks.each((_, element) => {
+        const $element = $(element) as JqueryElement;
+        const options = $element.data('slick');
+        $element.slick(options);
+    });
+    const $head = $('head');
+    const $link = document.createElement('link');
+    $link.rel = 'stylesheet';
+    $link.type = 'text/css';
+    $link.href = 'slick-style.css';
+    $head.append($link);
+});
 
 ReactDOM.render(
     <App />,
